a20981aa0e2c04c7a500edccb13fe15197380f27
Egoitz <egoitz@fisify.com>
2026-03-27 07:54:44 -0600

    Add AXA Keralty presentation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import ClientPresentationApp from './components/ClientPresentationApp'
+import AxaKeraltyApp from './pages/AxaKeraltyApp'
 import PrudentialApp from './pages/PrudentialApp'
 import AIGApp from './pages/AIGApp'
 import ZurichApp from './pages/ZurichApp'
@@ -8,13 +9,15 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<PrudentialApp />} />
+        <Route path="/" element={<AxaKeraltyApp />} />
+        <Route path="/axa-keralty" element={<AxaKeraltyApp />} />
+        <Route path="/axa" element={<AxaKeraltyApp />} />
+        <Route path="/medicus" element={<ClientPresentationApp clientName="Medicus" clientLogo="/images/medicus-logo-white.png" homePath="/medicus" />} />
         <Route path="/prudential" element={<PrudentialApp />} />
         <Route path="/AIG" element={<AIGApp homePath="/AIG" />} />
         <Route path="/aig" element={<AIGApp homePath="/AIG" />} />
         <Route path="/Zurich" element={<ZurichApp homePath="/Zurich" />} />
         <Route path="/zurich" element={<ZurichApp homePath="/zurich" />} />
-        <Route path="/medicus" element={<ClientPresentationApp clientName="Medicus" clientLogo="/images/medicus-logo-white.png" homePath="/medicus" />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/pages/AxaKeraltyApp.tsx
@@ -0,0 +1,36 @@
+import ClientPresentationAxa from '../components/ClientPresentationAxa'
+import type { TabId } from '../components/ClientPresentationApp'
+
+const CLIENT_NAME = 'AXA Keralty'
+const CLIENT_LOGO = '/images/axa-keralty-logo.svg'
+
+export default function AxaKeraltyApp() {
+  const visibleTabs: TabId[] = ['objetivo', 'results', 'product', 'value', 'implement']
+
+  return (
+    <ClientPresentationAxa
+      clientName={CLIENT_NAME}
+      clientLogo={CLIENT_LOGO}
+      homePath="/"
+      visibleTabs={visibleTabs}
+      initialTab="objetivo"
+      hero={{
+        eyebrow: 'Propuesta de colaboración',
+        title: (
+          <>
+            Prevención, bienestar y valor cotidiano para <span className="font-semibold">AXA Keralty</span>
+          </>
+        ),
+        description:
+          'Fisify convierte la fisioterapia digital en una experiencia de prevención y bienestar que el asegurado usa en su día a día y asocia al valor del seguro.',
+        image: '/images/new-hero-2.png',
+        imageAlt: 'AXA Keralty Fisioterapia Digital',
+      }}
+      footer={{
+        description:
+          'Fisify ayuda a tangibilizar el valor del seguro con una experiencia de prevención y bienestar que el asegurado utiliza en su día a día.',
+        bottomText: '© 2026 Fisify. Propuesta de colaboración para AXA Keralty.',
+      }}
+    />
+  )
+}
